7c48a9c4f1a982d2078596a62726db817c4c5275
fclesio <flavioclesio@yahoo.com>
2017-10-12 09:42:01 -0300

    Hello World excample

---
+++ Code/lectures/scala/helloWorld.scala
@@ -0,0 +1,6 @@
+//Programa em escala. Deve ser criado, e salvo em um .scala
+object HelloWorld { //Cria um objeto no Scala
+   def main(args: Array[String]) { //Cria uma funcao que retorna o "Hello world"
+      println("Hello, world!")
+   }
+}

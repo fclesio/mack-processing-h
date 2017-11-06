7123e4b9272bd3dd7b2dcd6fd6ac36af5baa9702
fclesio <flavioclesio@yahoo.com>
2017-11-06 21:27:13 -0200

    Exercicios do verao 2017

--- Code/lectures/scala/exercicios-do-verao.scala
+++ Code/lectures/scala/exercicios-do-verao.scala
@@ -12,30 +12,26 @@
 // 11) Quantas pessoas têm mais de 40 anos?
 // 12) Quantas pessoas têm entre 30 e 40 anos?
 // 13) Realize o agrupamento das pessoas pela idade de acordo com a quantidade.
-// 14) 
-// 15) 
-// 16) 
-// 17) 
-// 18) 
-// 19) 
-// 20) 
+// 14) Quantas pessoas tem entre 18 e 21 anos?
+// 15) Que pessoa que tem mais de 42 anos?
+
 
 // Dataframe: brasileiro_2015.json
 // 01) Qual foi o time que terminou em décimo terceiro lugar? 
-// 02)
-// 03)
+// 02) Quais times ficaram com menos de 50 cartões amarelos (GA) e que não ficaram entre os 6 primeiros?
+// 03) Quais foram os times com mais cartões vermelhos (GV) dos classificados para a libertadores (i.e. do primeiro até o sexto colocado)?
 // 04) Qual foi a média de pontos dos times que terminaram entre a décima segunda e a décima oitava posição?
 // 05) Quantos cartões amarelos os times com mais de 50 pontos e menos de 60 tiveram? 
 // 06) Quais foram os 3 primeiros times que tomaram cartão vermelho no torneio?
 // 07) Quais foram os 3 times que tomaram menos cartões amarelos?
 // 08) Quem foi o campeão do torneio?
-// 09)
+// 09) Qual foi a média de carões vermelhos (GV) do campeonato?
 // 10) Qual foi a média de gols pró (GP) dos times que terminaram entre o sexto lugar e o décimo terceiro lugar e que tiveram mais de 35 pontos?
-// 11) Quem foi o time que tomou menos cartões amarelos (GV) dos rebaixados?
+// 11) Quem foi o time que tomou menos cartões amarelos (GA) dos rebaixados?
 // 12) Quem foi o time que ficou com mais de 40 pontos, com menos de 30 gols pró (GP)?
-// 13)
+// 13) Qual foi a média de cartões amarelos (GV) do campeonato?
 // 14) Qual foi o time que terminou em último lugar?
-// 15)
+// 15) Agrupe os cartões amarelos de acordo com a sua quantidade?
 // 16) Qual é a quantidade cartões amarelos (GA) distribuídos no total?
 // 17) Qual foi a média de pontos entre os times que terminaram acima do sexto lugar?
 // 18) Quantos pontos em média fizeram os classificados para a libertadores (i.e. entre o campeão e o sexto lugar)?

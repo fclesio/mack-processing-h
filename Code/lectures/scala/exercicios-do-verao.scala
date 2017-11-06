b7ae71387826a71916362e87c0b491381cfa38c7
fclesio <flavioclesio@yahoo.com>
2017-11-06 21:14:54 -0200

    Exercicios do verao 2017

---
+++ Code/lectures/scala/exercicios-do-verao.scala
@@ -0,0 +1,14 @@
+// 1) Realizar o load em um dataframe chamado df_people
+// 2) Criar uma view temporária através do nosso dataframe chamada "pessoas"
+// 3) Selecione todas as pessoas que tenham mais de 40 anos
+// 4) Selecione todas as pessoas com menos de 21 anos
+// 5) Selecione todas as pessoas que tenham entre 27 anos e 29 anos
+// 6) Selecione todas as pessoas que tenham acima de 50 anos
+// 7) Veja qual é a idade média das pessoas que têm menos de 25 anos
+// 8) Veja qual é a idade média das pessoas que têm entre 30 anos e 42 anos
+// 9) Qual é a média de idade das pessoas com mais de 35 anos?
+// 10) Quantas pessoas têm menos de 25 anos?
+// 11) Quantas pessoas têm mais de 40 anos?
+// 12) Quantas pessoas têm entre 30 e 40 anos?
+// 13) Realize o agrupamento das pessoas pela idade de acordo com a quantidade.
+ 
